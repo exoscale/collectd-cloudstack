742fd3a85616b13336d096aadac3ab4f0e7135b6
Antoine Coetsier <acr@veltigroup.com>
2013-05-28 00:29:21 +0300

    incorporate resquest class in code for less dependencies

--- cloudstack.py
+++ cloudstack.py
@@ -12,11 +12,78 @@ import hmac
 import base64
 import hashlib
 import re
-import CloudStack
 
+class BaseClient(object):
+    def __init__(self, api, apikey, secret):
+        self.api = api
+        self.apikey = apikey
+        self.secret = secret
+
+    def request(self, command, args):
+        args['apikey']   = self.apikey
+        args['command']  = command
+        args['response'] = 'json'
+        
+        params=[]
+        
+        keys = sorted(args.keys())
+
+        for k in keys:
+            params.append(k + '=' + urllib.quote_plus(args[k]).replace("+", "%20"))
+       
+        query = '&'.join(params)
+
+        signature = base64.b64encode(hmac.new(
+            self.secret, 
+            msg=query.lower(), 
+            digestmod=hashlib.sha1
+        ).digest())
+
+        query += '&signature=' + urllib.quote_plus(signature)
+
+        response = urllib2.urlopen(self.api + '?' + query)
+        decoded = json.loads(response.read())
+       
+        propertyResponse = command.lower() + 'response'
+        if not propertyResponse in decoded:
+            if 'errorresponse' in decoded:
+                raise RuntimeError("ERROR: " + decoded['errorresponse']['errortext'])
+            else:
+                raise RuntimeError("ERROR: Unable to parse the response")
+
+        response = decoded[propertyResponse]
+        result = re.compile(r"^list(\w+)s").match(command.lower())
+
+        if not result is None:
+            type = result.group(1)
+
+            if type in response:
+                return response[type]
+            else:
+                # sometimes, the 's' is kept, as in :
+                # { "listasyncjobsresponse" : { "asyncjobs" : [ ... ] } }
+                type += 's'
+                if type in response:
+                    return response[type]
+
+        return response
+
+class Client(BaseClient):
+	def listHosts(self, args={}):
+		return self.request('listHosts', args)
+        
+        def listCapabilities(self, args={}):
+        	return self.request('listCapabilities', args)
+        
+        def listCapacity(self, args={}):
+        	return self.request('listCapacity', args)
+        
+        def listSystemVms(self, args={}):
+        	return self.request('listSystemVms', args)
+        
 NAME = 'cloudstack'
 
-DEFAULT_API = 'http://localh:8096/client/api'
+DEFAULT_API = 'http://localhost:8096/client/api'
 DEFAULT_AUTH = False
 DEFAULT_APIKEY = ''
 DEFAULT_SECRET = ''
